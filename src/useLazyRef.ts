2b408ccd54c5765311c771025b5f9f3024276c29
Nghiệp <me@nghiepit.dev>
2021-10-30 14:16:02 +0700

    Assert non-null return type

--- src/useLazyRef.ts
+++ src/useLazyRef.ts
@@ -1,10 +1,12 @@
-import {useRef, MutableRefObject} from 'react';
+import {useRef} from 'react';
 
 const useLazyRef = <T>(initialValFunc: () => T) => {
-    const ref: MutableRefObject<T | null> = useRef(null);
+    const ref = useRef<T>(null!);
+    
     if (ref.current === null) {
         ref.current = initialValFunc();
     }
+    
     return ref;
 };
 
